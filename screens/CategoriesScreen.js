86b8a725e6da762a277813d37a83e258cee172b8
MaxRobertsDear <maxhroberts@gmail.com>
2020-02-19 13:07:00 +0000

    testing git-hooks

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const CategoriesScreen = props => {
-  <View>
+const CategoriesScreen = () => {
+  <View style={styles.screen}>
     <Text>This is the Categories Screen</Text>
   </View>;
 };
 
-styles = StyleSheet.create({
+const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    textAlign: 'center',
   },
 });
 
--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const CategoryMealScreen = props => {
-  <View>
+const CategoryMealScreen = () => {
+  <View style={styles.screen}>
     <Text>This is the Categories Meal Screen</Text>
   </View>;
 };
 
-styles = StyleSheet.create({
+const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
